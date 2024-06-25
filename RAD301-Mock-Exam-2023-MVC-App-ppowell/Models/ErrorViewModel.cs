b93caea78debb7508e3f25903c3b0b1f170af291
Paul Powell <paul.powell@atu.ie>
2024-06-25 12:24:19 +0100

    Add project files.

---
+++ RAD301-Mock-Exam-2023-MVC-App-ppowell/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RAD301_Mock_Exam_2023_MVC_App_ppowell.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
