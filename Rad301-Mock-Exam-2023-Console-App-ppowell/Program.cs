f97e0b2667a64372e93e5eed3622bb0ee4e1cc94
Paul Powell <paul.powell@atu.ie>
2024-06-25 12:28:08 +0100

    ppowell Part a complete. 10 Marks for project setup and configuration.  NOTE: Solution and all projects targets .net 7.0 All EF packages target .NET 7.20 (latest version of .NET 7)

--- Rad301-Mock-Exam-2023-Console-App-ppowell/Program.cs
+++ Rad301-Mock-Exam-2023-Console-App-ppowell/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace Rad301_Mock_Exam_2023_Console_App_ppowell
+﻿using Tracker.WebAPIClient;
+
+namespace Rad301_Mock_Exam_2023_Console_App_ppowell
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ActivityAPIClient.Track(StudentID: "ppowell", StudentName: "Paul Powell", activityName: "Rad301 Mock Exam 2023", Task: "Exam Start");
         }
     }
 }
